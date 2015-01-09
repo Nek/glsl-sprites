ab48dd59c211732decf8c7c0ba484bbd2e0d1977
Nikita Dudnik <nikdudnik@gmail.com>
2015-01-10 02:30:15 +0300

    Update fragment shader
    Fix new names for time and resolution.
    Fix aspect ration problems.

--- balls.glsl
+++ balls.glsl
@@ -1,38 +1,37 @@
-  precision mediump float;
-  uniform vec2 resolution;
-  uniform float time;
+precision mediump float;
 
-  #define MAX_BALLS 10
+uniform vec2 uScreenSize;
+uniform float uTime;
 
-  struct Ball {
-    vec2 center;
-    float radius;
-  };
+#define MAX_BALLS 10
 
-  uniform Ball balls[MAX_BALLS];
-  uniform int ballsLength;
+struct Ball {
+  vec2 center;
+  float radius;
+};
 
-  bool inCircle (vec2 p, vec2 center, float radius) {
-    vec2 ratio = resolution.xy / resolution.x;
-    return distance(p*ratio, center*ratio) < radius;
-  }
+uniform Ball balls[MAX_BALLS];
+uniform int ballsLength;
 
-  bool inBall(vec2 p, Ball b) {
-    return inCircle(p, b.center, b.radius);
-  }
+bool inCircle (vec2 p, vec2 center, float radius) {
+  vec2 ratio = uScreenSize.xy / uScreenSize.x;
+  return distance(p*ratio, center*ratio) < radius;
+}
 
-  void main () {
-    vec2 p = gl_FragCoord.xy / resolution.xy;
-    float z = 0.5+0.5*smoothstep(-1.0, 1.0, cos(time * 0.005));
-    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+bool inBall(vec2 p, Ball b) {
+  return inCircle(p, b.center, b.radius);
+}
 
-    for (int i=0; i<MAX_BALLS; ++i) { if (i>=ballsLength) break;
-      if (inBall(p, balls[i])) {
-        gl_FragColor = vec4(p.x, p.y, z, 1.0);
-      }
+void main () {
+
+  vec2 position = (gl_FragCoord.xy / uScreenSize.xy);
+
+  float z = 0.5+0.5*smoothstep(-1.0, 1.0, cos(uTime * 0.005));
+  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+
+  for (int i=0; i<MAX_BALLS; ++i) { if (i>=ballsLength) break;
+    if (inBall(position, balls[i])) {
+      gl_FragColor = vec4(position.x, position.y, z, 1.0);
     }
   }
-
-
-
-
+}
